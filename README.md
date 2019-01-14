20b675cd5d7cc3eb27cbeebcca0a6936eecc85ff
Michael Barrett <twisterghost@gmail.com>
2019-01-13 19:42:08 -0500

    Cleanup, define engines in package.json

--- README.md
+++ README.md
@@ -9,41 +9,17 @@ $ git clone ...
 $ npm i
 ```
 
-Then, populate two json files:
-
-**players.json** which contains a JSON array of unique player names as strings:
-
-```json
-[
-  "someone",
-  "someoneElse"
-]
-```
-
-> **Note**: These are used as object keys in JS and therefore must be valid object keys
-
-**games.json** which contains a JSON array of outcomes, following the format:
-
-```json
-[
-  // Completed Game
-  {
-    "players": ["someone", "somoneElse"],
-    "startDate": "1/1/2019",
-    "endDate": "1/2/2019",
-    "winner": "someoneElse"
-  },
-  // Active Game
-  {
-    "players": ["somebody", "somebodyElse"],
-    "startDate": "1/1/2019"
-  }
-  ...
-]
-```
-
 ## Run
 
 `npm start`
 
 It will serve on port 3000 using [micro](https://github.com/zeit/micro)
+
+## Endpoints
+
+* `/`
+  * View Elo rankings
+* `/addPlayer`
+  * Add a player to the system
+* `/addResult`
+  * Add a game result to the system
--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const EloRank = require('elo-rank');
 const elo = new EloRank(25);
 const DEFAULT_ELO = 1000;
 const moment = require('moment');
-const DATE_FORMAT = 'M/D/YYYY';
 
+const DATE_FORMAT = 'M/D/YYYY';
 const NAME_REGEX = /^[a-zA-Z][a-zA-Z_]{1,29}$/;
 
 // players.json is a json array of strings representing unique player names
@@ -42,12 +42,19 @@ try {
   console.info('Using default game list data');
 }
 
+/**
+ * Saves the data to local files
+ */
 function save() {
   console.info('Saving data files...');
   fs.writeFileSync('./games.json', JSON.stringify(gameList), 'utf8');
   fs.writeFileSync('./players.json', JSON.stringify(playerList), 'utf8');
 }
 
+/**
+ * Calculates elo ranks from scratch using the current data
+ * @returns {object} key-value pairs of player names to elo rank
+ */
 function calculate() {
 
   const rankings = {}
@@ -70,9 +77,9 @@ function calculate() {
     game.players.forEach((player, idx) => {
       // TODO: If we support more than 2 players in a game, then we'll need to update this logic, but elo should support it
       const otherIdx = 1 - idx; // Trick to get the opposite index of the current one (1 -> 0 | 0 -> 1)
-      const otherPlyr = game.players[otherIdx];
+      const otherPlayer = game.players[otherIdx];
       const prevRating = rankings[player];
-      const expectedScore = elo.getExpected(prevRating, rankings[otherPlyr]);
+      const expectedScore = elo.getExpected(prevRating, rankings[otherPlayer]);
       const actualScore = (game.winner === player) ? 1 : 0;
       rankings[player] = elo.updateRating(expectedScore, actualScore, prevRating);
     });
@@ -81,11 +88,20 @@ function calculate() {
   return rankings;
 }
 
+/**
+ * Given an object of elo rankings, get a list rank objects ordered by rank
+ * @param rankings {object} Rankings object as created by `calculate()`
+ * @returns {orderedRanks} A list of objects containing player name and elo in order of elo
+ */
 function asOrderedTuples(rankings) {
   const asTuples = Object.keys(rankings).map(player => ({player, elo: rankings[player]}));
   return asTuples.sort((a, b) => b.elo - a.elo);
 }
 
+/**
+ * Generates the html for the elo ranking page
+ * @returns {string}
+ */
 function sendRankHtml() {
   const rankings = asOrderedTuples(calculate());
   let response = '<html><a href="/addPlayer">Add Player</a> - <a href="/addResult">Add Result</a><br />'
@@ -97,6 +113,10 @@ function sendRankHtml() {
   return response;
 }
 
+/**
+ * Generates the html for the /addResult endpoint
+ * @returns {string}
+ */
 function addResultForm() {
   const playerOptions = playerList.map(player => {
     return `<option>${player}</option>`;
@@ -121,6 +141,13 @@ function addResultForm() {
     `;
 }
 
+/**
+ * Adds a game result to the games list
+ * @async
+ * @param req {request} the request object from micro
+ * @param res {response} the response object from micro
+ * @returns {send result|string}
+ */
 async function addResult(req, res) {
   const { winner, loser } = await parse(req);
   if (!playerList.includes(winner) || !playerList.includes(loser) || winner == loser) {
@@ -131,12 +158,16 @@ async function addResult(req, res) {
     players: [winner, loser],
     startDate: moment().format(DATE_FORMAT),
     endDate: moment().format(DATE_FORMAT),
-    winner: winner
+    winner,
   });
   save();
-  return `<html>Added game result with winner of ${winner} and loser of ${loser}. <a href="/addResult">Add another?</a></html>`;
+  return `<html>Added game result with winner of ${winner} and loser of ${loser}. <a href="/addResult">Add another</a> or <a href="/">view rankings</a></html>`;
 }
 
+/**
+ * Generates the html for the /addPlayer endpoint
+ * @returns {string}
+ */
 function addPlayerForm() {
   return `
     <html>
@@ -150,12 +181,19 @@ function addPlayerForm() {
     `;
 }
 
+/**
+ * Adds a player to the player list
+ * @async
+ * @param req {request} the request object from micro
+ * @param res {response} the response object from micro
+ * @returns {string}
+ */
 async function addPlayer(req) {
   const { player } = await parse(req);
   if (NAME_REGEX.test(player)) {
     playerList = _.uniq(playerList.concat(player));
     save();
-    return `<html>Added ${player}. <a href="/addPlayer">Add another?</a></html>`;
+    return `<html>Added ${player}. <a href="/addPlayer">Add another</a> or <a href="/">view rankings</a></html>`;
   } else {
     return `<html>Player "${player}" not added - names must be letters and underscores only (starting with a letter) and between 1 and 30 characters. <a href="/addPlayer">Try Again?</a></html>`;
   }
